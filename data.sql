0c2896acb21f7c773e5bc49c309e12a10e295fd2
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-12-06 23:48:43 +0200

    Add database code

--- data.sql
+++ data.sql
@@ -1,5 +1,40 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals 
+ (
+    name,
+    date_of_birth,
+    escape_attempts, 
+    neutered,
+    weight_kg
+  ) 
+    VALUES
+  (
+    'Agumon',
+     TO_DATE('2020-02-03', 'YYYY-MM-DD'),
+     0,
+     TRUE,
+     10.23
+   ),
+   (
+    'Gabumon',
+     TO_DATE('2018-11-15', 'YYYY-MM-DD'),
+     2,
+     FALSE,
+     15.04
+   ),
+   (
+    'Pikachu',
+     TO_DATE('2021-1-7', 'YYYY-MM-DD'),
+     1,
+     FALSE,
+     15.04
+   ),
+   (
+    'Devimon',
+     TO_DATE('2017-05-12', 'YYYY-MM-DD'),
+     5,
+     TRUE,
+     11.00
+   );
+   
